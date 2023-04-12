ad1f055cb857bb0cea08da1fc6d0d47b7080f97c
arifshaikhvk25 <arifshaikhvk25@gmail.com>
2023-04-13 02:03:57 +0530

    updated Contact field EmailJS

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,20 +1,52 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from '@emailjs/browser';
 import "./Footer.css"
 import { img } from "../../imports";
 import { FiFacebook, FiTwitter, FiInstagram } from "react-icons/fi"
 
 
 const Footer = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_ymwp9tx', 'template_35swpfp', form.current, 'YwjTTHUdurBHRDbrZ')
+            .then((result) => {
+                console.log(result.text);
+                e.target.reset()    // this will reset once it submits
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
     return (
         <div className="footer" id="contact">
             <div className="footer-Heading">
                 <h3 className="footer-H3tag">Join our email list and get access to specials deals exclusive to our subscribers.</h3>
             </div>
 
-            <div className="footer-Email">
+            {/* <div className="footer-Email">
                 <input type="email" name="" id="" placeholder='Enter Your Email' />
                 <button type='button'> Let's Get Started</button>
-            </div>
+            </div> */}
+
+            <form ref={form} onSubmit={sendEmail} className="form">
+
+                <label>Name</label>
+                <input type="text" name="user_name" autoComplete="off" required style={{marginBottom: "1rem"}} placeholder="Enter Your Name" />
+
+                <label>Email</label>
+                <input type="email" name="user_email" autoComplete="off" required style={{marginBottom: "1rem"}} placeholder="Enter Your Email"  />
+
+                <label>Message</label>
+                <textarea name="message" cols="30" rows="6" autoComplete="off" required style={{marginBottom: "1rem"}} placeholder="Enter Your Text" />
+
+                {/* <button type='submit' value="Send" > Let's Get Started</button> */}
+                <button type='submit' value="Send" > Submit </button>
+
+            </form>
 
             <div className="footer-Content">
 
